a139e40c996f46acac5de534c4105f5d9424210f
Victor Temprano <tempranova@gmail.com>
2024-09-25 19:38:09 -0700

    trying another prisma method

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client'
+import { Polygon, User } from "@prisma/client";
 import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3'
 import { Readable } from 'stream'
 
