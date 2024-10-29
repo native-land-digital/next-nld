1f2240ed2bc47421f5737dac6718ed58864df557
Victor Temprano <tempranova@gmail.com>
2024-10-28 17:53:06 -0700

    uncommenting seed

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -108,150 +108,150 @@ async function main() {
     });
 
     // Removing template that came along with export
-    // let templateIndex = importJSON.findIndex(row => row.name.includes("Template"));
-    // if(typeof templateIndex !== undefined && templateIndex > -1) {
-    //   importJSON.splice(templateIndex, 1);
-    // }
-    //
-    // let createdRecords = 0;
-    //
-    // for await (const row of importJSON) {
-    //
-    //   // Ensuring unique slug
-    //   let slug = JSON.parse(JSON.stringify(row.slug));
-    //   const slugSuffix = "-";
-    //   let slugNumber = 1;
-    //   let slugIsUnique = false;
-    //   while(!slugIsUnique) {
-    //     const currentSlug = slug + (slugNumber > 1 ? (slugSuffix + slugNumber.toString()) : "")
-    //     const foundSlug = await prisma.entry.findUnique({
-    //       where : {
-    //         slug : currentSlug
-    //       }
-    //     })
-    //     if(!foundSlug) {
-    //       slugIsUnique = true;
-    //       slug = currentSlug;
-    //     } else {
-    //       slugNumber = slugNumber + 1;
-    //     }
-    //   }
-    //
-    //   let newEntry = await prisma.entry.create({
-    //     data : {
-    //       createdAt : new Date(row.createdAt),
-    //       updatedAt : new Date(row.updatedAt),
-    //       name : row.name,
-    //       slug : slug,
-    //       color : row.color,
-    //       sources : row.sources,
-    //       category : row.category,
-    //       published : true,
-    //       pronunciation : row.pronunciation ? row.pronunciation : "",
-    //       websites : {
-    //         createMany : {
-    //           data : row.websites.map(website => {
-    //             return {
-    //               url : website.url,
-    //               title : website.title
-    //             }
-    //           })
-    //         }
-    //       },
-    //       changelog : {
-    //         createMany : {
-    //           data : row.changelog.map(change => {
-    //             return {
-    //               createdAt : new Date(change.createdAt),
-    //               description : change.description
-    //             }
-    //           })
-    //         }
-    //       },
-    //       media : {
-    //         createMany : {
-    //           data : row.media.map(thisMedia => {
-    //             return {
-    //               url : `${process.env.AWS_WP_CLOUDFRONT}/${thisMedia.url}`,
-    //               title : thisMedia.title,
-    //               caption : thisMedia.caption
-    //             }
-    //           })
-    //         }
-    //       }
-    //     },
-    //     select : {
-    //       id : true
-    //     }
-    //   });
-    //
-    //   console.log(row.name)
-    //
-    //   // Then add the geometry
-    //   if(newEntry) {
-    //     createdRecords = createdRecords + 1;
-    //     if(row.geometry) {
-    //       if(row.geometry.type.indexOf("Polygon") > -1) {
-    //         await prisma.$executeRawUnsafe(`
-    //           INSERT INTO "Polygon" (geometry, "entryId")
-    //           VALUES (ST_Force2D(ST_GeomFromGeoJSON('${JSON.stringify(row.geometry)}')), ${newEntry.id})
-    //         `)
-    //       }
-    //     }
-    //   } else {
-    //     console.log('MISSED AN ENTRY')
-    //   }
-    //
-    // }
-    //
-    // console.log('created ', createdRecords);
-    //
-    // // Then add the related fields
-    // const entries = await prisma.entry.findMany({
-    //   select : {
-    //     id : true,
-    //     slug : true
-    //   }
-    // });
-    //
-    // for await (const row of importJSON) {
-    //   let thisEntry = entries.find(entry => entry.slug === row.slug)
-    //   if(thisEntry) {
-    //     let hasRelation = false;
-    //     row.related.forEach(thisRelation => {
-    //       let relatedEntry = entries.find(entry => entry.slug === thisRelation.relatedTo_slug)
-    //       if(relatedEntry) {
-    //         hasRelation = true;
-    //       }
-    //     })
-    //     if(hasRelation) {
-    //
-    //       let limitedRelatedToInner : RelationInner[] = [];
-    //       row.related.map(thisRelation => {
-    //         let relatedEntry = entries.find(entry => entry.slug === thisRelation.relatedTo_slug)
-    //         if(relatedEntry) {
-    //           limitedRelatedToInner.push({
-    //             description : thisRelation.description,
-    //             relatedToId : relatedEntry.id
-    //           })
-    //         }
-    //       })
-    //
-    //       let newEntryRelation = await prisma.entry.update({
-    //         where : {
-    //           id : thisEntry.id
-    //         },
-    //         data : {
-    //           relatedTo : {
-    //             createMany : {
-    //               data : limitedRelatedToInner
-    //             }
-    //           }
-    //         }
-    //       });
-    //     }
-    //   }
-    // }
+    let templateIndex = importJSON.findIndex(row => row.name.includes("Template"));
+    if(typeof templateIndex !== undefined && templateIndex > -1) {
+      importJSON.splice(templateIndex, 1);
+    }
+
+    let createdRecords = 0;
+
+    for await (const row of importJSON) {
+
+      // Ensuring unique slug
+      let slug = JSON.parse(JSON.stringify(row.slug));
+      const slugSuffix = "-";
+      let slugNumber = 1;
+      let slugIsUnique = false;
+      while(!slugIsUnique) {
+        const currentSlug = slug + (slugNumber > 1 ? (slugSuffix + slugNumber.toString()) : "")
+        const foundSlug = await prisma.entry.findUnique({
+          where : {
+            slug : currentSlug
+          }
+        })
+        if(!foundSlug) {
+          slugIsUnique = true;
+          slug = currentSlug;
+        } else {
+          slugNumber = slugNumber + 1;
+        }
+      }
+
+      let newEntry = await prisma.entry.create({
+        data : {
+          createdAt : new Date(row.createdAt),
+          updatedAt : new Date(row.updatedAt),
+          name : row.name,
+          slug : slug,
+          color : row.color,
+          sources : row.sources,
+          category : row.category,
+          published : true,
+          pronunciation : row.pronunciation ? row.pronunciation : "",
+          websites : {
+            createMany : {
+              data : row.websites.map(website => {
+                return {
+                  url : website.url,
+                  title : website.title
+                }
+              })
+            }
+          },
+          changelog : {
+            createMany : {
+              data : row.changelog.map(change => {
+                return {
+                  createdAt : new Date(change.createdAt),
+                  description : change.description
+                }
+              })
+            }
+          },
+          media : {
+            createMany : {
+              data : row.media.map(thisMedia => {
+                return {
+                  url : `${process.env.AWS_WP_CLOUDFRONT}/${thisMedia.url}`,
+                  title : thisMedia.title,
+                  caption : thisMedia.caption
+                }
+              })
+            }
+          }
+        },
+        select : {
+          id : true
+        }
+      });
+
+      console.log(row.name)
+
+      // Then add the geometry
+      if(newEntry) {
+        createdRecords = createdRecords + 1;
+        if(row.geometry) {
+          if(row.geometry.type.indexOf("Polygon") > -1) {
+            await prisma.$executeRawUnsafe(`
+              INSERT INTO "Polygon" (geometry, "entryId")
+              VALUES (ST_Force2D(ST_GeomFromGeoJSON('${JSON.stringify(row.geometry)}')), ${newEntry.id})
+            `)
+          }
+        }
+      } else {
+        console.log('MISSED AN ENTRY')
+      }
+
+    }
+
+    console.log('created ', createdRecords);
+
+    // Then add the related fields
+    const entries = await prisma.entry.findMany({
+      select : {
+        id : true,
+        slug : true
+      }
+    });
+
+    for await (const row of importJSON) {
+      let thisEntry = entries.find(entry => entry.slug === row.slug)
+      if(thisEntry) {
+        let hasRelation = false;
+        row.related.forEach(thisRelation => {
+          let relatedEntry = entries.find(entry => entry.slug === thisRelation.relatedTo_slug)
+          if(relatedEntry) {
+            hasRelation = true;
+          }
+        })
+        if(hasRelation) {
+
+          let limitedRelatedToInner : RelationInner[] = [];
+          row.related.map(thisRelation => {
+            let relatedEntry = entries.find(entry => entry.slug === thisRelation.relatedTo_slug)
+            if(relatedEntry) {
+              limitedRelatedToInner.push({
+                description : thisRelation.description,
+                relatedToId : relatedEntry.id
+              })
+            }
+          })
+
+          let newEntryRelation = await prisma.entry.update({
+            where : {
+              id : thisEntry.id
+            },
+            data : {
+              relatedTo : {
+                createMany : {
+                  data : limitedRelatedToInner
+                }
+              }
+            }
+          });
+        }
+      }
+    }
   }
 
 }
