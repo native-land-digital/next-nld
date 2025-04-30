f0b15e86532c129d361919e590bb01b6568f08b0
Victor Temprano <tempranova@gmail.com>
2025-04-29 21:29:39 -0700

    schema updates

--- prisma/seed/seed-places.ts
+++ prisma/seed/seed-places.ts
@@ -18,100 +18,109 @@ interface Row {
   geometry : Geometry;
 }
 
+const placenamesToFetch = [ "navajo", "dakota", "yupik", "apache", "keres", "cherokee", "choctaw", "zuni", "oodham", "ojibwe", "hopi", "inupiat", "tewa", "muskogee", "crow", "shoshoni", "cheyenne", "tiwa", "towa", "inuktitut", "central_siberian_yupik", "central_alaskan_yupik", "alutiiq", "unangan", "denaina", "deg_xinag", "holikachuk", "koyukon", "dinaki", "gwichin", "lower_tanana", "upper_tanana", "tanacross", "han", "ahtna", "eyak", "tlingit", "haida", "smalgyax", "northern_sami", "lule_sami", "southern_sami", "skolt_sami", "pite_sami", "kven", "maori", "cree", "innu_aimun", "chipewyan", "oji_cree", "mikmaq", "sioux", "atikamekw", "blackfoot", "tlicho", "algonquin", "dakelh", "gitxsanimaax", "tsilhqotin", "slavey", "maliseet_passamaquoddy", "inuinnaqtun", "kanienkeha", "pitcairn_norfolk", "australian_aboriginal_language", "yugambeh_bandjalangic", "pitjantjatjara", "wiradjuri", "nyungar", "gundungurra", "pintupi", "bininj_gun_wok", "ngarigo", "daungwurrung", "woiwurrung", "wathawurrung", "kaurna", "gunditjmara", "eastern_arrernte", "dharug" ]
+
 async function main() {
 
-  let createdRecords = 0;
-
-  // Get appropriate seed file from AWS bucket
-  const client = new S3Client({ region: process.env.AWS_REGION })
-  const seedBucketParams = { Bucket: process.env.AWS_SEED_BUCKET_NAME, Key: process.env.AWS_SEED_PLACENAME_FILE };
-  const data = await client.send(new GetObjectCommand(seedBucketParams));
-
-  if(data && data.Body) {
-    const importString = await data.Body.transformToString();
-    let importJSON = <Row[]>JSON.parse(importString)
-
-    if(importJSON) {
-
-      // importJSON.splice(10); // For import testing
-
-      for await (const row of importJSON) {
-
-        // Ensuring unique slug
-        let slug = encodeURIComponent(JSON.parse(JSON.stringify(row.in_lang))).toLowerCase();
-        const slugSuffix = "-";
-        let slugNumber = 1;
-        let slugIsUnique = false;
-        while(!slugIsUnique) {
-          const currentSlug = slug + (slugNumber > 1 ? (slugSuffix + slugNumber.toString()) : "")
-          const foundSlug = await prisma.entry.findUnique({
-            where : {
-              slug : currentSlug
-            }
-          })
-          if(!foundSlug) {
-            slugIsUnique = true;
-            slug = currentSlug;
-          } else {
-            slugNumber = slugNumber + 1;
-          }
-        }
-
-        let newEntry = await prisma.entry.create({
-          data : {
-            createdAt : new Date(),
-            updatedAt : new Date(),
-            name : row.in_lang,
-            slug : slug,
-            sources : `<p>OpenStreetMaps reference: <a href="https://www.openstreetmap.org/${row.osm_id}" target="_blank">${row.name}</a>`,
-            category : 'placenames',
-            published : true,
-            pronunciation : row.in_lang_latin ? row.in_lang_latin : "",
-            websites : {
-              createMany : {
-                data : [{
-                  url : `https://www.openstreetmap.org/${row.osm_id}`,
-                  title : "OpenStreetMaps Node"
-                }]
-              }
-            },
-            changelog : {
-              createMany : {
-                data : [{
-                  createdAt : new Date(),
-                  description : "Placename added"
-                }]
-              }
-            },
-          },
-          select : {
-            id : true
-          }
-        });
-
-        console.log(row.name)
-
-        // Then add the geometry
-        if(newEntry) {
-          createdRecords = createdRecords + 1;
-          if(row.geometry) {
-            if(row.geometry.type.indexOf("Point") > -1) {
-              await prisma.$executeRawUnsafe(`
-                INSERT INTO "Point" (geometry, "entryId", "osmType", "osmId")
-                VALUES (ST_Force2D(ST_GeomFromGeoJSON('${JSON.stringify(row.geometry)}')), ${newEntry.id}, '${row.place_type}', '${row.osm_id}')
-              `)
-            }
-          }
-        } else {
-          console.log('MISSED AN ENTRY')
-        }
-
-        // Could have something here associating it with a given language, or an overlapping nation polygon, or some combination of those things?
-      }
-
-      console.log(`created ${createdRecords} rows`)
+  await prisma.entry.deleteMany({ where : {
+    category : {
+      equals : 'placenames'
     }
-  }
+  } })
+
+  // let createdRecords = 0;
+  //
+  // // Get appropriate seed file from AWS bucket
+  // const client = new S3Client({ region: process.env.AWS_REGION })
+  // const seedBucketParams = { Bucket: process.env.AWS_SEED_BUCKET_NAME, Key: `placenames/cree.json` };
+  // const data = await client.send(new GetObjectCommand(seedBucketParams));
+  //
+  // if(data && data.Body) {
+  //   const importString = await data.Body.transformToString();
+  //   let importJSON = <Row[]>JSON.parse(importString)
+  //
+  //   if(importJSON) {
+  //
+  //     // importJSON.splice(10); // For import testing
+  //
+  //     for await (const row of importJSON) {
+  //
+  //       // Ensuring unique slug
+  //       let slug = encodeURIComponent(JSON.parse(JSON.stringify(row.in_lang))).toLowerCase();
+  //       const slugSuffix = "-";
+  //       let slugNumber = 1;
+  //       let slugIsUnique = false;
+  //       while(!slugIsUnique) {
+  //         const currentSlug = slug + (slugNumber > 1 ? (slugSuffix + slugNumber.toString()) : "")
+  //         const foundSlug = await prisma.entry.findUnique({
+  //           where : {
+  //             slug : currentSlug
+  //           }
+  //         })
+  //         if(!foundSlug) {
+  //           slugIsUnique = true;
+  //           slug = currentSlug;
+  //         } else {
+  //           slugNumber = slugNumber + 1;
+  //         }
+  //       }
+  //
+  //       let newEntry = await prisma.entry.create({
+  //         data : {
+  //           createdAt : new Date(),
+  //           updatedAt : new Date(),
+  //           name : row.in_lang,
+  //           slug : slug,
+  //           sources : `<p>OpenStreetMaps reference: <a href="https://www.openstreetmap.org/${row.osm_id}" target="_blank">${row.name}</a>`,
+  //           category : 'placenames',
+  //           published : true,
+  //           pronunciation : row.in_lang_latin ? row.in_lang_latin : "",
+  //           language :
+  //           websites : {
+  //             createMany : {
+  //               data : [{
+  //                 url : `https://www.openstreetmap.org/${row.osm_id}`,
+  //                 title : "OpenStreetMaps Node"
+  //               }]
+  //             }
+  //           },
+  //           changelog : {
+  //             createMany : {
+  //               data : [{
+  //                 createdAt : new Date(),
+  //                 description : "Placename added"
+  //               }]
+  //             }
+  //           }
+  //         },
+  //         select : {
+  //           id : true
+  //         }
+  //       });
+  //
+  //       console.log(row.name)
+  //
+  //       // Then add the geometry
+  //       if(newEntry) {
+  //         createdRecords = createdRecords + 1;
+  //         if(row.geometry) {
+  //           if(row.geometry.type.indexOf("Point") > -1) {
+  //             await prisma.$executeRawUnsafe(`
+  //               INSERT INTO "Point" (geometry, "entryId", "osmType", "osmId")
+  //               VALUES (ST_Force2D(ST_GeomFromGeoJSON('${JSON.stringify(row.geometry)}')), ${newEntry.id}, '${row.place_type}', '${row.osm_id}')
+  //             `)
+  //           }
+  //         }
+  //       } else {
+  //         console.log('MISSED AN ENTRY')
+  //       }
+  //
+  //       // Could have something here associating it with a given language, or an overlapping nation polygon, or some combination of those things?
+  //     }
+  //
+  //     console.log(`created ${createdRecords} rows`)
+  //   }
+  // }
 
 }
 
