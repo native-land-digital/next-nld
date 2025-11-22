98d9727d9de8ec92ec77c410c84f9f444a3e9cc9
Victor Temprano <tempranova@gmail.com>
2025-11-22 15:30:48 +0100

    territory point tileset generation

--- src/components/dashboard/update-mapbox.jsx
+++ src/components/dashboard/update-mapbox.jsx
@@ -23,6 +23,7 @@ export default function UpdateMapbox() {
       <p className="md:col-span-4">{t('mapbox-update')}</p>
       <div className="grid grid-cols-1 md:grid-cols-6 mt-2.5 h-20">
         <button className="mt-2.5 md:mt-0 md:col-span-1 border bg-gray-100 hover:bg-gray-300 border-gray-300 px-4 py-3 rounded md:ml-2.5" onClick={() => updateMapbox('territories')}>Update Territories</button>
+        <button className="mt-2.5 md:mt-0 md:col-span-1 border bg-gray-100 hover:bg-gray-300 border-gray-300 px-4 py-3 rounded md:ml-2.5" onClick={() => updateMapbox('territories-points')}>Update Territories Points</button>
         <button className="mt-2.5 md:mt-0 md:col-span-1 border bg-gray-100 hover:bg-gray-300 border-gray-300 px-4 py-3 rounded md:ml-2.5" onClick={() => updateMapbox('languages')}>Update Languages</button>
         <button className="mt-2.5 md:mt-0 md:col-span-1 border bg-gray-100 hover:bg-gray-300 border-gray-300 px-4 py-3 rounded md:ml-2.5" onClick={() => updateMapbox('treaties')}>Update Treaties</button>
         <button className="mt-2.5 md:mt-0 md:col-span-1 border bg-gray-100 hover:bg-gray-300 border-gray-300 px-4 py-3 rounded md:ml-2.5" onClick={() => updateMapbox('greetings')}>Update Greetings</button>
