53ceaab18c265cb054e00bd830324008f117ff46
Victor Temprano <tempranova@gmail.com>
2025-11-29 08:56:46 +0100

    first test redesign

---
+++ src/app/[locale]/(public)/maps/nations/page.jsx
@@ -0,0 +1,58 @@
+import { db } from '@/lib/db/kysely'
+import { availableLocales } from '@/i18n/config'
+import { setLocaleCache } from '@/i18n/server-i18n';
+
+import MapContainer from '@/components/maps/nations/map-container';
+import AIChatbot from '@/components/ai/chatbot';
+
+// import defaultContent from "./en.mdx"
+
+export const revalidate = false;
+export const dynamic = 'force-static';
+export const dynamicParams = false;
+
+export function generateStaticParams() {
+  return availableLocales.map((locale) => ({ locale }));
+}
+
+export default async function Home({ params : { locale } }) {
+
+  setLocaleCache(locale);
+
+  // Querying for select2 list initial options
+  const territoryOptions = await db.selectFrom('Entry')
+    .where('category', '=', 'territories')
+    .where('published', '=', true)
+    .select(['id', 'name'])
+    .limit(25)
+    .execute()
+
+  const languageOptions = await db.selectFrom('Entry')
+    .where('category', '=', 'languages')
+    .where('published', '=', true)
+    .select(['id', 'name'])
+    .limit(25)
+    .execute()
+
+  const treatyOptions = await db.selectFrom('Entry')
+    .where('category', '=', 'treaties')
+    .where('published', '=', true)
+    .select(['id', 'name'])
+    .limit(25)
+    .execute()
+
+  // let Content = defaultContent;
+  // try {
+  //   const TranslatedContent = (await import(`./${locale}.mdx`)).default;
+  //   Content = TranslatedContent;
+  // } catch {
+
+  // }
+
+  return (
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <MapContainer territoryOptions={territoryOptions} languageOptions={languageOptions} treatyOptions={treatyOptions} />
+      <AIChatbot />
+    </div>
+  );
+}
---
+++ src/components/maps/nations/map-container.jsx
@@ -0,0 +1,76 @@
+"use client";
+import { useEffect, useState } from "react";
+
+import MapModal from '@/components/maps/nations/modal';
+import Map from '@/components/maps/nations/map';
+import SelectorControl from "@/components/maps/nations/map-selector-control";
+import TogglesControl from "@/components/maps/nations/map-toggles-control";
+
+export default function MapContainer({
+  territoryOptions,
+  languageOptions,
+  treatyOptions,
+}) {
+  const allLayers = ["territories"];
+  const [map, setMap] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedFeatures, setSelectedFeatures] = useState([]);
+  const [currentLayers, setCurrentLayers] = useState(["territories"]);
+
+  useEffect(() => {
+    document.querySelector("body").classList.add("no-footer");
+  }, [])
+
+  useEffect(() => {
+    // Autoload from front page search
+    if(map) {
+      map.on('load', () => {
+        const urlParams = new URLSearchParams(window.location.search);
+        
+        const categoryParam = urlParams.get('category');
+        if(categoryParam) {
+          setCurrentLayers([categoryParam]);
+        }
+
+        const bboxParam = urlParams.get('bbox');
+        const centerParam = urlParams.get('center');
+        if(bboxParam && centerParam) {
+          map.fitBounds(JSON.parse(bboxParam), { duration : 0 });
+          setTimeout(() => {
+            const coords = centerParam.split(',');
+            const latlng = { lat : coords[1], lng : coords[0] }
+            map.fire('click', { latLng : latlng, point: map.project(latlng), originalEvent : {} });
+          }, 1000);
+        }
+      })
+    }
+  }, [map])
+
+  return (
+    <div className="w-90 h-dvh min-h-120 relative">
+      {currentLayers.indexOf("greetings") > -1 ?
+        <MapModal setModalOpen={setModalOpen} modalOpen={modalOpen} headerText="greetings-disclaimer-header" bodyText="greetings-disclaimer" footerText="greetings-disclaimer-close" />
+      : false}
+      <MapModal setModalOpen={setModalOpen} modalOpen={modalOpen} headerText="disclaimer-header" bodyText="disclaimer" footerText="disclaimer-close" />
+      <SelectorControl
+        allLayers={allLayers}
+        map={map}
+        currentLayers={currentLayers}
+        setCurrentLayers={setCurrentLayers}
+        selectedFeatures={selectedFeatures}
+        setSelectedFeatures={setSelectedFeatures}
+        territoryOptions={territoryOptions}
+        languageOptions={languageOptions}
+        treatyOptions={treatyOptions}
+      />
+      <TogglesControl allLayers={allLayers} map={map} setModalOpen={setModalOpen} />
+      <Map
+        allLayers={allLayers}
+        map={map}
+        currentLayers={currentLayers}
+        setMap={setMap}
+        setSelectedFeatures={setSelectedFeatures}
+      />
+    </div>
+  );
+}
